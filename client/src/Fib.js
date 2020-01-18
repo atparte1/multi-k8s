4be9da086956a24963ed8225797b80993b123b5f
atparte1 <atparte1@in.ibm.com>
2020-01-18 15:44:55 +0530

    First commit

---
+++ client/src/Fib.js
@@ -0,0 +1,71 @@
+import React, { Component} from 'react';
+import axios from 'axios';
+
+class Fib extends Component{
+    state ={
+        seenIndexes: [],
+        values: {},
+        index: ''
+    };
+
+    componentDidMount(){
+        this.fetchValues();
+        this.fetchIndexes();
+    }
+
+    async fetchValues(){
+        const values = await axios.get('/api/values/current');
+        console.log('values');
+        this.setState({
+             values: values.data
+            });
+    }
+    async fetchIndexes(){
+        const seenIndexes = await axios.get('/api/values/all');
+        this.setState({
+            seenIndexes: seenIndexes.data
+        });
+    }
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        await axios.post('/api/values', {
+            index: this.state.index
+        });
+        this.setState({index: ''});
+    };
+    redenderSeendIndexes(){
+        return this.state.seenIndexes.map(({number}) => number).join(', ');
+    }
+
+    rederValues(){
+        const entries = [];
+
+        for(let key in this.state.values){
+            entries.push(
+                <div key={key}>
+                 for index {key} I calculated {this.state.values[key]}
+                 </div>
+            )
+        }
+    }
+    render(){
+        return(
+            <div>
+                <form onSubmit ={this.handleSubmit}>
+                    <label>Enter your index:</label>
+                    <input 
+                        value={this.state.index}
+                        onChange={event => this.setState({ index: event.target.value })}
+                    />
+                    <button>Submit</button>
+                </form>
+                <h3>Index I have seen:</h3>
+                {this.redenderSeendIndexes()}
+                <h3>Calculated values</h3>
+                {this.rederValues()}
+            </div>
+        )
+    }
+}
+
+export default Fib;
