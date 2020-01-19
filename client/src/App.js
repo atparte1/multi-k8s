04552037e0c38c380443254e031d188f8be833f2
atparte1 <atparte1@in.ibm.com>
2020-01-19 13:02:09 +0530

    change master

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-Title">Welcome To Fibo calc with  a difference</h1>
+          <h1 className="App-Title">Welcome To best Fibo calc with  a difference</h1>
           <Link to="/">Fibo Calculator</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
