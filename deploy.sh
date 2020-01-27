f165a9c86cba88f0ec4bfb504476a558614ba1be
atparte1 <atparte1@in.ibm.com>
2020-01-27 13:47:27 +0530

    changed Deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+chmod 0755 ./client/node_modules/.bin/jest
+chmod +x ./client/node_modules/.bin/react-scripts
 docker build -t atparte1/multi-client:latest -t atparte1/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t atparte1/multi-server:latest -t atparte1/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t atparte1/multi-worker:latest -t atparte1/multi-worker:$SHA -f ./worker/Dockerfile ./worker
