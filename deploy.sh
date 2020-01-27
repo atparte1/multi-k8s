8dda3f0da3d86dc786fb5b9f4f3bb20f8519796d
atparte1 <atparte1@in.ibm.com>
2020-01-27 12:49:12 +0530

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t atparte1/multi-client:latest -t atparte1/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t atparte1/multi-server:latest -t atparte1/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t atparte1/multi-worker:latest -t atparte1/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push atparte1/multi-client:latest
+docker push atparte1/multi-server:latest
+docker push atparte1/multi-worker:latest
+
+docker push atparte1/multi-client:$SHA
+docker push atparte1/multi-server:$SHA
+docker push atparte1/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployment/server-deployment server=atparte1/multi-server:$SHA
+kubectl set image deployment/client-deployment client=atparte1/multi-client:$SHA
+kubectl set image deployment/worker-deployment worker=atparte1/multi-worker:$SHA
